85218305a317d75c655af2b9266e2e3f705cb4a3
Ihvaan <akyurek.ihvan@gmail.com>
2024-05-17 16:59:50 +0200

    First commit

---
+++ src/User.java
@@ -0,0 +1,14 @@
+public class User {
+    public static void main(String[] args) {
+        CandyCrushLeaderboard leaderboard = CandyCrushLeaderboard.getInstance();
+
+        leaderboard.addScore("player3", 150);
+        leaderboard.addScore("player4", 250);
+
+        System.out.println("Scores: " + leaderboard.getScores());
+
+        CandyCrushLeaderboard anotherLeaderboard = CandyCrushLeaderboard.getInstance();
+        System.out.println("Same instance: " + (leaderboard == anotherLeaderboard));
+    }
+}
+
